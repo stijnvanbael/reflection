ce6c4c57172a920d20db5ed8ddba69e272463f72
Stijn Van Bael <stijnvanbael@gmail.com>
2015-01-20 17:04:05 +0100

    Added Optional

--- lib/reflective.dart
+++ lib/reflective.dart
@@ -2,7 +2,7 @@ library reflective;
 
 import 'dart:mirrors';
 import 'dart:convert';
-import 'dart:collection';
 
 part 'src/reflective.dart';
 part 'src/convert.dart';
+part 'src/optional.dart';
---
+++ lib/src/optional.dart
@@ -0,0 +1,96 @@
+part of reflective;
+
+abstract class Optional<T> {
+  const Optional();
+
+  factory Optional.of(T value) {
+    return value == null ? empty : new Present(value);
+  }
+
+  T get();
+
+  T orElse(T other);
+
+  T orNull();
+
+  T or(T supplier());
+
+  Optional map(dynamic mapper(T value));
+
+  Optional expand(Optional mapper(T value));
+
+  Optional<T> where(bool predicate(T value));
+
+  List<T> toList();
+
+  Optional<T> ifPresent(void handler(T value));
+
+  Optional<T> ifAbsent(void handler());
+
+  bool isPresent();
+}
+
+const empty = const Empty();
+
+class Empty<T> extends Optional<T> {
+  const Empty();
+
+  T get() => throw new AbsentException();
+
+  T orElse(T other) => other;
+
+  T orNull() => null;
+
+  T or(T supplier()) => supplier();
+
+  Optional map(dynamic mapper(T value)) => this;
+
+  Optional expand(Optional mapper(T value)) => this;
+
+  Optional<T> where(bool predicate(T value)) => this;
+
+  List<T> toList() => [];
+
+  Optional<T> ifPresent(void handler(T value)) => this;
+
+  Optional<T> ifAbsent(void handler()) {
+    handler();
+    return this;
+  }
+
+  bool isPresent() => false;
+}
+
+class Present<T> extends Optional<T> {
+  T value;
+
+  Present(this.value);
+
+  T get() => value;
+
+  T orElse(T other) => value;
+
+  T orNull() => value;
+
+  T or(T supplier()) => value;
+
+  Optional map(dynamic mapper(T value)) => new Optional.of(mapper(value));
+
+  Optional expand(Optional mapper(T value)) => mapper(value);
+
+  Optional<T> where(bool predicate(T value)) => predicate(value) ? this : empty;
+
+  List<T> toList() => [value];
+
+  Optional<T> ifPresent(void handler(T value)) {
+    handler(value);
+    return this;
+  }
+
+  Optional<T> ifAbsent(void handler()) => this;
+
+  bool isPresent() => true;
+}
+
+class AbsentException {
+}
