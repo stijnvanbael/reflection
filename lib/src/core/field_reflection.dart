b6eb39f37566158331810f618cc99349db29b6ef
Jake Rote <jrote1@me.com>
2016-07-07 10:57:29 +0100

    Added isConst to field

--- lib/src/core/field_reflection.dart
+++ lib/src/core/field_reflection.dart
@@ -3,6 +3,8 @@ part of reflective.core;
 abstract class FieldReflection {
   bool get isGeneric;
 
+  bool get isConst;
+
   bool has(Type metadata);
 
   List metadata(Type metadata);
@@ -38,6 +40,8 @@ class SimpleFieldReflection extends AbstractReflection<VariableMirror> with Fiel
 
   bool get isGeneric => _mirror.type is TypeVariableMirror;
 
+  bool get isConst => _mirror.isConst;
+
   bool operator ==(o) =>
       o is FieldReflection && fullName == o.fullName && _symbol == o._symbol;
 }
@@ -69,4 +73,6 @@ class TransitiveFieldReflection implements FieldReflection {
   bool get isPrivate => _target.isPrivate;
 
   bool get isGeneric => false;
+
+  bool get isConst => _target.isConst;
 }
